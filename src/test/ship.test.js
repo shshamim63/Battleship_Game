d28371f305e7c9caafa3f685ccef49cb202355b6
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-12-03 21:34:39 +0600

    adds test for checking hit function which changes the actual position

--- src/test/ship.test.js
+++ src/test/ship.test.js
@@ -6,4 +6,10 @@ describe('Ship', () => {
   test('contains a ship length', () => {
     expect(ship.shipLength).toBe(3);
   });
+  test('contains a hit function that takes a number and then marks that position', () => {
+    ship.hit(0);
+    ship.hit(1);
+    ship.hit(2);
+    expect(ship.isSunk()).toBe(true);
+  });
 });
