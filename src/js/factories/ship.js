afe045d0c644a87e4c48b8da013814d81e25b661
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-12-03 21:56:37 +0600

    adds ship object

---
+++ src/js/factories/ship.js
@@ -0,0 +1,15 @@
+const Ship = (len) => {
+  const shipPosition = new Array(len).fill('O');
+  const shipLength = len;
+  const hit = (position) => {
+    shipPosition[position] = 'X';
+  };
+  const isSunk = () => shipPosition.every((element) => element === 'X');
+  return {
+    shipPosition,
+    shipLength,
+    hit,
+    isSunk,
+  };
+};
+module.exports = Ship;
