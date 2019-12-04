b495fdfc23e41e2f78e468d559c33852ffe874a4
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-12-04 20:30:00 +0600

    adds test for ship placement

--- src/test/gameboard.test.js
+++ src/test/gameboard.test.js
@@ -8,4 +8,8 @@ describe('Gameboard', () => {
   test('has a board of row^2 length', () => {
     expect(gameboard.board.length).toBe(10 ** 2);
   });
+  test('places ship on the gameboard', () => {
+    gameboard.placeShips([4, 3, 2]);
+    expect(gameboard.board.filter((grid) => typeof grid === 'object').length).toBe(6);
+  });
 });
