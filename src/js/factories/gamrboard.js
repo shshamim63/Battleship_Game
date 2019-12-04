6ca1171e86c48e86f6e3168bb5b69bf6e6f2927b
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-12-04 23:01:33 +0600

    adds ship placement method

--- src/js/factories/gamrboard.js
+++ src/js/factories/gamrboard.js
@@ -1,9 +1,40 @@
+const Ship = require('./ship');
+
 const Gameboard = () => {
   const boardRow = 10;
   const board = new Array(boardRow ** 2).fill('O');
+  const shipContainer = [];
+  const shipOrientation = () => Math.random() >= 0.5;
+  const getRandomPosition = (position) => Math.random * position;
+  const positionFactor = (verticalOrientation, pos) => (verticalOrientation ? pos * boardRow : pos);
+  const shipPlacement = (position, shipLength, isVertical) => {
+    const currentShip = Ship(shipLength);
+    shipContainer.push(currentShip);
+    currentShip.shipPosition.forEach((element, idx) => {
+      board[position + positionFactor(isVertical, idx)] = {
+        currentShip,
+        shipPositionIndex: idx,
+        targetHit: false,
+      };
+    });
+  };
+  // const positionIsvalid = () => {
+
+  // };
+  const placeShips = (shipDiversity) => {
+    shipDiversity.forEach((element) => {
+      const isVertical = shipOrientation();
+      let randomPosition;
+      do {
+        randomPosition = getRandomPosition(board.length);
+      } while (!positionIsvalid(randomPosition, element, isVertical));
+      shipPlacement(randomPosition, element, isVertical);
+    });
+  };
   return {
     boardRow,
     board,
+    placeShips,
   };
 };
 module.exports = Gameboard;
