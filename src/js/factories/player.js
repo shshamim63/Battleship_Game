de468591f206a7b387b75f946b51ab6848f641d2
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-12-06 19:45:10 +0600

    adds attack method for players attack

--- src/js/factories/player.js
+++ src/js/factories/player.js
@@ -1,8 +1,23 @@
 const Player = () => {
   const validMoves = [];
+  const isArrayEqual = (first, second) => first.length === second.length
+    && first.every((v, i) => v === second[i]);
 
+  const allreadyPlayed = (first, second) => first.some(
+    (firstElem) => isArrayEqual(firstElem, second),
+  );
+  const attack = (gameboard, x, y) => {
+    if (!allreadyPlayed(validMoves, [x, y])) {
+      const res = gameboard.receiveAttack(x, y);
+      validMoves.push([x, y]);
+      return res;
+    }
+    return false;
+  };
   return {
     validMoves,
+    attack,
+    allreadyPlayed,
   };
 };
 module.exports = Player;
